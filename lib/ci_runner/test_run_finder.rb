ef9b0163bf4ceb4476d7a8c880ff2d09887bd286
Edouard CHIN <chin.edouard@gmail.com>
2022-08-01 15:23:21 +0200

    Documentation

--- lib/ci_runner/test_run_finder.rb
+++ lib/ci_runner/test_run_finder.rb
@@ -4,6 +4,17 @@ module CIRunner
   module TestRunFinder
     extend self
 
+    # Makes a request to GitHub to retrieve the checks for a commit. Display a nice UI with
+    # a spinner while the user wait.
+    #
+    # @param repository [String] The full repository name, including the owner (i.e. rails/rails)
+    # @param commit [String] The Git commit that has been pushed to GitHub and for which we'll retrieve the CI checks.
+    # @param block [Proc, Lambda] A proc that will be called in case we can't retrieve the CI Checks.
+    #   This allows the CLI to prematurely exit and let the CLI::UI closes its frame.
+    #
+    # @return [Hash] See GitHub documentation
+    #
+    # @see https://docs.github.com/en/rest/checks/runs#list-check-runs-for-a-git-reference
     def fetch_ci_checks(repository, commit, &block)
       github_client = GithubClient.new(Configuration::User.instance.github_token)
       ci_checks = {}
@@ -22,6 +33,17 @@ module CIRunner
       ci_checks
     end
 
+    # Find the CI check the user requested from the list of upstream checks.
+    # This method is useful only when the user passes the `--run-name` flag to `ci-runner`. This makes
+    # sure the CI check actually exists.
+    #
+    # @param ci_checks [Hash] The response from the previous +fetch_ci_checks+ request.
+    # @param run_name [String] The name of the CI run that the user would like to retry on its machine.
+    #
+    # @return [Hash] A single check run from the list of +ci_checks+
+    #
+    # @raise [Error] If no CI checks with the given +run_name+ could be found.
+    # @raise [Error] If the CI check was successfull. No point to continue as there should be no tests to rerun.
     def find(ci_checks, run_name)
       check_run = ci_checks["check_runs"].find { |check_run| check_run["name"] == run_name }
       raise(Error.new(no_check_message(ci_checks, run_name))) if check_run.nil?
@@ -30,6 +52,16 @@ module CIRunner
       check_run
     end
 
+    # Try to guess which runner (Minitest, RSpec) was responsible for this log output.
+    #
+    # The runner is the most important part of CI Runner. It's what determine the failures for a CI log,
+    # as well as how to rerun those only.
+    #
+    # @param ci_log [String] The log output from CI.
+    #
+    # @return [Runners::MinitestRunner, Runners::RSpec]
+    #
+    # @raise [Error] In case none of the runners could detect the log output.
     def detect_runner(ci_log)
       raise_if_not_found = lambda { raise(Error, "Couldn't detect the test runner") }
 
@@ -42,10 +74,17 @@ module CIRunner
 
     private
 
+    # @param [String] run_name The name of the CI check input or chosen by the user.
+    #
+    # @return [String] A error message to display.
     def check_succeed(run_name)
       "The CI check '#{run_name}' was successfull. There should be no failing tests to rerun."
     end
 
+    # @param [Hash] ci_checks The list of CI checks previously by the +fetch_ci_checks+ method.
+    # @param [String] run_name The name of the CI check input or chosen by the user.
+    #
+    # @return [String] A error message letting the user know why CI Runner couldn't continue.
     def no_check_message(ci_checks, run_name)
       possible_checks = ci_checks["check_runs"].map do |check_run|
         if check_run["conclusion"] == "success"
